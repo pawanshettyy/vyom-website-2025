1e2635a1ac280ef225c02e6e4f70013a911f3549
rohandol112 <rohan45321dol@gmail.com>
2025-06-04 01:13:42 +0530

    check

--- Hermetica-Club-main/Hermetica-Club-main/Hermetica-Club-main/Hermetica-Club-main/actions/getProjects.ts
+++
@@ -1,9 +0,0 @@
-
-"use server";
-
-import { db } from "@/lib/db/db";
-import { projectsTable } from "@/lib/db/schema";
-
-export async function fetchProjects() {
-  return await db.select().from(projectsTable);
-}
--- Hermetica-Club-main/Hermetica-Club-main/config/env.ts
+++
@@ -1,4 +0,0 @@
-export const AUTH_GOOGLE_ID = process.env.AUTH_GOOGLE_ID || ""
-export const AUTH_GOOGLE_SECRET = process.env.AUTH_GOOGLE_SECRET || ""
-export const AUTH_SECRET = process.env.AUTH_SECRET || ""
-export const DATABASE_URL = process.env.DATABASE_URL || ""
